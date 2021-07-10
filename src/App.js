405f390bd01ff8ba25edfc4e662663a7f32eacd2
iamanmolmehra <iamanmolmehra@gmail.com>
2021-07-10 15:07:11 +0530

    I have wrote code on HOOKS, a concept used in func. concept of ReactJS

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import logo from './logo.svg';
+import './App.css';
+import ClassCounter from './Components/ClassCounter';
+import HookCounter from './Components/HookCounter';
+import HookCounterTwo from './Components/HookCounterTwo';
+import HookCounterThree from './Components/HookCounterThree';
+import HookCounterFour from './Components/HookCounterFour';
+import ClassCounterOne from './Components/ClassCounterOne';
+import HookCounterOne from './Components/HookCounterOne';
+import ClassMouse from './Components/ClassMouse';
+
+function App() {
+  return (
+    <div className="App">
+    <ClassMouse></ClassMouse>
+    {/* <HookCounterOne></HookCounterOne> */}
+    {/* <ClassCounterOne></ClassCounterOne> */}
+      {/* <HookCounterFour></HookCounterFour> */}
+      {/* <HookCounterThree></HookCounterThree> */}
+      {/* <HookCounterTwo></HookCounterTwo> */}
+      {/* <HookCounter></HookCounter> */}
+      {/* <ClassCounter></ClassCounter> */}
+    </div>
+  );
+}
+
+export default App;
